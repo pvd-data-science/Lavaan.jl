40aa764d2f7e553f578f80cbf3289f4e052d49ec
paulstey <paulstey@gmail.com>
2019-06-17 18:02:19 -0400

    feat(lav_syntax.jl): add struct definition for ParsedModel

---
+++ src/lav_syntax.jl
@@ -0,0 +1,17 @@
+# NOTE: Expect this file to be re-structured away in to a more general file 
+# that contains type definitions.
+
+struct ParsedModel 
+	lhs
+	op
+	rhs
+	mod_idx 
+	block
+	fixed
+	start 
+	lower
+	upper
+	label
+	prior
+	efa
+end  
