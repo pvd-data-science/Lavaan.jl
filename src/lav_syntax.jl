88b2b24615fb04c2227a8bc92b2b1ee3bfd16d74
paulstey <paulstey@gmail.com>
2019-06-17 18:33:35 -0400

    refactor(lav_syntax.jl): add functionality for parsing model string

--- src/lav_syntax.jl
+++ src/lav_syntax.jl
@@ -2,16 +2,31 @@
 # that contains type definitions.
 
 struct ParsedModel 
-	lhs
-	op
-	rhs
-	mod_idx 
-	block
-	fixed
-	start 
-	lower
-	upper
-	label
-	prior
-	efa
+    lhs
+    op
+    rhs
+    mod_idx 
+    block
+    fixed
+    start 
+    lower
+    upper
+    label
+    prior
+    efa
 end  
+
+
+function lav_parse_model_string(model_syntax::String)
+    # Remove comments up to newline, and keep newline
+    model_syntax = replace(model_syntax, r"[#!].*(?=\n)" => "")
+
+    # Replace semi-colon with newline
+    model_syntax = replace(model_syntax, ";", "\n")
+
+    # Remove all whitespace 
+    model_syntax = replace(model_syntax, "[ \t]+", "")
+
+end 
+
+
