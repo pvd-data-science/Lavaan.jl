647bbcac12a01689be2a6ad4d01687a90e464f6e
paulstey <paulstey@gmail.com>
2019-06-17 21:18:15 -0400

    refactor(lav_syntax.jl): added a few more parsing steps

--- src/lav_syntax.jl
+++ src/lav_syntax.jl
@@ -22,11 +22,30 @@ function lav_parse_model_string(model_syntax::String)
     model_syntax = replace(model_syntax, r"[#!].*(?=\n)" => "")
 
     # Replace semi-colon with newline
-    model_syntax = replace(model_syntax, ";", "\n")
+    model_syntax = replace(model_syntax, ";" => "\n")
 
     # Remove all whitespace 
-    model_syntax = replace(model_syntax, "[ \t]+", "")
+    model_syntax = replace(model_syntax, "[ \t]+" => "")
 
+    # Remove occurrence of >= 2 consecutive newlines to eliminate blank 
+    # statements; this retains blank newline at the beginning, if such exists, 
+    # but parser will not choke because of start.idx
+    model_syntax = replace(model_syntax, "\n{2,}" => "\n")
+    
+    # break up in lines
+    model = split(model_syntax, "\n")
+
+    # check for multi-line formulas: they contain no operator symbol
+    # but before we do that, we remove all strings between double quotes
+    # to avoid confusion with for example equal("f1=~x1") statements
+    # model.simple <- gsub("\\(.*\\)\\*", "MODIFIER*", model)
+    model_simple <- replace(model, r"\\\".[^\\\"]*\\\"" => "LABEL")
+
+    operators = ["=~", "<~", "~*~", "~~", "~", "==", "<", ">", ":=", ":", "\\|", "%"]
+    lhs_modifiers = ["efa"]
+    operators_extra = vcat(operators, lhs_modifiers)
+
+    model
 end 
 
 
